cd15424786adcd84a1f96e662ea39c83a97c6d85
Rafael Santos <formigarafa@gmail.com>
2013-06-25 16:41:25 +1200

    reformat exhibition of ruby versions.

--- versions.rb
+++ versions.rb
@@ -2,6 +2,11 @@
 
 require File.expand_path('../ruby_version', __FILE__)
 
+Status = {
+  alert: 0x2757.chr('UTF-8'),
+  tick: 0x2705.chr('UTF-8')
+}
+
 installed_rubies = `rbenv versions --bare`.lines.collect(&:strip)
 installed_ruby_versions = installed_rubies.map {|v| RubyVersion.parse v }.group_by(&:implementation_version)
 
@@ -9,20 +14,38 @@ available_rubies = `rbenv install -l`.lines.map(&:strip)[1..-1]
 available_ruby_versions = available_rubies.map {|v| RubyVersion.parse v }.group_by(&:implementation_version)
 
 ruby_versions_info = installed_ruby_versions.inject({}) do |latest, implementation_info|
-  latest[implementation_info[0]] ={installed: implementation_info[1].max, available: available_ruby_versions[implementation_info[0]].max}
+  latest_installed = implementation_info[1].max
+  latest_available = available_ruby_versions[implementation_info[0]].max
+
+  latest[implementation_info[0]] = {
+    installed: implementation_info[1].max, 
+    available: available_ruby_versions[implementation_info[0]].max,
+    status: latest_available > latest_installed ? :alert : :tick
+  }
   latest
 end
-
 
 require 'yaml'
 
 info = {
   # "description" => RUBY_DESCRIPTION,
+  "testar" => ["brew outdated"],
   "rubies" => ruby_versions_info,
   "ruby-build" => `ruby-build --version`.strip,
   "rbenv" => `rbenv --version`.strip,
   "brew" => `brew --version`.strip,
 }
 
-puts info.to_yaml
+info["rubies"].each do |k, v|
+  description = if v[:status] == :tick
+    v[:installed].release
+  else
+    "#{v[:installed].release} < #{v[:available].release}"
+  end
 
+  puts "#{Status[v[:status]]} #{v[:installed].implementation_version}: #{description}"
+end
+
+info.delete "rubies"
+
+puts info.to_yaml
